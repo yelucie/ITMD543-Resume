c8aa357ffcfc4d5dd9be65d9ed782de8f81c6417
yelucie <lucie.ye@reseau.eseo.fr>
2023-10-03 14:32:23 -0500

    INIT: some customization on landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome on the landing page of Lucie YE's resume website.
         </p>
         <a
           className="App-link"
